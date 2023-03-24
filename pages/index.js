06e4196d38238942536c68640730bf5dc11dd1e9
Sara Haghiri <sarahaghiri88@gmail.com>
2023-03-24 12:39:46 +0430

    add secounddescription page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Header from '../components/header';
 import Main from '../components/main';
 import FDescription from '../components/fdescription';
+import SDescription from '../components/sdescription';
 const index = () => {
   return (
     <div>
       <Header />
       <Main />
       <FDescription />
+      <SDescription />
     </div>
   );
 };
