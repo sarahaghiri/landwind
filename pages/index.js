84bc957aaa144349ca728ebe2ea47d514aaca421
Sara Haghiri <sarahaghiri88@gmail.com>
2023-03-24 13:08:24 +0430

    add quote page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Header from '../components/header';
 import Main from '../components/main';
 import FDescription from '../components/fdescription';
 import SDescription from '../components/sdescription';
+import Quote from '../components/quote';
 const index = () => {
   return (
     <div>
@@ -9,6 +10,7 @@ const index = () => {
       <Main />
       <FDescription />
       <SDescription />
+      <Quote />
     </div>
   );
 };
