1df2c644c91a368c27f70c8a666bb6c915a565fb
Sara Haghiri <sarahaghiri88@gmail.com>
2023-03-24 13:37:43 +0430

    craete card

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Main from '../components/main';
 import FDescription from '../components/fdescription';
 import SDescription from '../components/sdescription';
 import Quote from '../components/quote';
+import Card from '../components/card';
 const index = () => {
   return (
     <div>
@@ -11,6 +12,7 @@ const index = () => {
       <FDescription />
       <SDescription />
       <Quote />
+      <Card />
     </div>
   );
 };
